2c76ecc42ce88e1d96da139279303b7e16533e68
Carlo Leonardi <carloleonardi83@gmail.com>
2017-12-17 11:56:50 +0100

    add extract3

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ exports.extract = function() {
 exports.extract2 = function() {
     return parseInt(Math.random()*6) + 1;
 }
+
+exports.extract3 = function() {
+    return parseInt(Math.random()*6) + 1;
+}
